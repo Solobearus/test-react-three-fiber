dd9f2b1b212cc378474eec31959e4fc81d23105f
ivan solodov <ivansolobear@gmail.com>
2022-03-31 18:38:26 +0300

    in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Player } from "./Components/Player";
-import {Physics} from "@react-three/cannon";
-import {Ground} from "./Components/Ground";
-import {PointerLockControls, Sky} from "@react-three/drei";
+import {Debug, Physics} from "@react-three/cannon";
+import { Ground } from "./Components/Ground";
+import { PointerLockControls, Sky } from "@react-three/drei";
 
 function App() {
   return (
@@ -13,14 +13,11 @@ function App() {
         <ambientLight intensity={0.3} />
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
         <Physics gravity={[0, -30, 0]}>
-          <Player />
-          <Ground />
+          <Debug color="black" scale={1.1}>
+            <Player />
+            <Ground />
+          </Debug>
         </Physics>
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial />
-        </mesh>
-        <PointerLockControls />
       </Canvas>
     </div>
   );
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,62 +1,154 @@
-import * as THREE from "three"
-import { useEffect, useRef, useState } from "react"
-import { useSphere } from "@react-three/cannon"
-import { useThree, useFrame } from "@react-three/fiber"
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { Physics, useBox, useSphere } from "@react-three/cannon";
+import { useThree, useFrame, Canvas } from "@react-three/fiber";
+import { PerspectiveCamera, PointerLockControls } from "@react-three/drei";
+import { useResource } from "react-three-fiber";
 
-const SPEED = 5
-const keys = { KeyW: "forward", KeyS: "backward", KeyA: "left", KeyD: "right", Space: "jump" }
-const moveFieldByKey = (key) => keys[key]
-const direction = new THREE.Vector3()
-const frontVector = new THREE.Vector3()
-const sideVector = new THREE.Vector3()
+const SPEED = 5;
+const keys = {
+  KeyW: "forward",
+  KeyS: "backward",
+  KeyA: "left",
+  KeyD: "right",
+  Space: "jump",
+};
+const moveFieldByKey = (key) => keys[key];
+const direction = new THREE.Vector3();
+const frontVector = new THREE.Vector3();
+const sideVector = new THREE.Vector3();
 
 const usePlayerControls = () => {
-  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false })
+  const [movement, setMovement] = useState({
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+    jump: false,
+  });
   useEffect(() => {
-    const handleKeyDown = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
-    const handleKeyUp = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
-    document.addEventListener("keydown", handleKeyDown)
-    document.addEventListener("keyup", handleKeyUp)
+    const handleKeyDown = (e) =>
+      setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }));
+    const handleKeyUp = (e) =>
+      setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }));
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
     return () => {
-      document.removeEventListener("keydown", handleKeyDown)
-      document.removeEventListener("keyup", handleKeyUp)
-    }
-  }, [])
-  return movement
-}
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+  return movement;
+};
 
-const useStateMachine = ({forward, backward, left, right, jump, api, ref}) => {
+const playerMovementGround = ({
+  playerControls,
+  api,
+  ref,
+  camera,
+  velocity,
+  currentPos,
+}) => {
+  const { forward, backward, left, right, jump } = playerControls;
+
+  frontVector.set(0, 0, Number(backward) - Number(forward));
+  sideVector.set(Number(left) - Number(right), 0, 0);
+
+  direction
+    .subVectors(frontVector, sideVector)
+    .normalize()
+    .multiplyScalar(SPEED)
+    .applyEuler(camera.rotation);
+
+
+  const newPos = new THREE.Vector3(0, 2, 10);
+  newPos.applyEuler(camera.rotation)
+
+  newPos.x += currentPos.current[0]
+  newPos.y += currentPos.current[1]
+  newPos.z += currentPos.current[2]
+
+  camera.position.set(newPos.x,newPos.y,newPos.z);
+
+  const cameraReorderedRotation = camera.rotation.reorder("YZX");
+  console.log(cameraReorderedRotation)
+
+  api.rotation.set(1,1,1);
+  // api.rotation.set(camera.rotation.x,camera.rotation.y,camera.rotation.z);
+  api.velocity.set(direction.x, velocity.current[1], direction.z);
+
+  if (jump) api.velocity.set(velocity.current[0], 10, velocity.current[2]);
+};
+
+const playerMovementFly = ({ playerControls, api, camera, velocity }) => {
+  const { forward, backward, left, right, jump } = playerControls;
+
+  frontVector.set(0, 0, Number(backward) - Number(forward));
+  sideVector.set(Number(left) - Number(right), 0, 0);
+  direction
+    .subVectors(frontVector, sideVector)
+    .normalize()
+    .multiplyScalar(SPEED)
+    .applyEuler(camera.rotation);
+
+  api.velocity.set(direction.x, velocity.current[1], direction.z);
+  if (jump) api.velocity.set(velocity.current[0], 10, velocity.current[2]);
+};
+
+const useStateMachine = () => {
   const currentState = "onGround";
 
-  const { camera } = useThree()
-  const velocity = useRef([0, 0, 0])
-  useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [])
-
-  useFrame(() => {
-    if(currentState === "onGround"){
-      ref.current.getWorldPosition(camera.position)
-
-      frontVector.set(0, 0, Number(backward) - Number(forward))
-      sideVector.set(Number(left) - Number(right), 0, 0)
-      direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(camera.rotation)
-
-      api.velocity.set(direction.x, velocity.current[1], direction.z)
-      if (jump) api.velocity.set(velocity.current[0], 10, velocity.current[2])
-    } else {
-
-    }
-  })
-}
+  if (currentState === "onGround") {
+    return playerMovementGround;
+  } else {
+    return playerMovementFly;
+  }
+};
 
 export const Player = (props) => {
-  const [ref, api] = useSphere(() => ({ mass: 0.1, type: "Dynamic", position: [0, 1, 0], ...props }))
-  const playerControls = usePlayerControls()
+  const [camera, setCamera] = useState();
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    type: "Dynamic",
+    position: [0, 1, 0],
+    angularFactor: [0, 1, 0],
+    ...props,
+  }));
 
-  useStateMachine({...playerControls , api, ref});
+  const currentPos = useRef([0, 0, 0]);
+  const velocity = useRef([0, 0, 0]);
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => (velocity.current = v));
+    api.position.subscribe((pos) => {
+      currentPos.current = pos;
+    });
+  }, []);
+
+  const playerControls = usePlayerControls();
+  const currentStateFunction = useStateMachine();
+
+  useFrame(() => {
+    currentStateFunction({
+      playerControls,
+      api,
+      ref,
+      camera,
+      velocity,
+      currentPos,
+    });
+  });
 
   return (
-      <>
-        <mesh ref={ref} />
-      </>
-  )
-}
+    <>
+      <group ref={ref} position={[0, 0, 0]}>
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </group>
+      <PerspectiveCamera makeDefault position={[0, 2, 10]} ref={setCamera} />
+      {camera && <PointerLockControls camera={camera} />}
+    </>
+  );
+};
